c6c08943c729ea63beab8700b608606d9cfaa219
ariani86 <40794971+ariani86@users.noreply.github.com>
2018-07-24 20:04:33 -0400

    Jul 24th latest app

--- server.R
+++ server.R
@@ -34,7 +34,7 @@ function(input, output, session) {
     region_2 <- if (is.null(input$country)) character(0) else {
       cleantable %>%
         filter(Country %in% input$country,
-          is.null(input$region_1) | Region1 %in% input$region_1) %>%
+          is.null(input$province) | Province %in% input$province) %>%
         `$`('Region2') %>%
         unique() %>%
         sort()
@@ -47,8 +47,8 @@ function(input, output, session) {
   observe({
     winery <- if (is.null(input$country)) character(0) else {
       cleantable %>%
-        filter(Province %in% input$province,
-          is.null(input$region_2) | Region2 %in% input$region_2) %>%
+        filter(Country %in% input$country,
+          is.null(input$province) | Province %in% input$province) %>%
         `$`('Winery') %>%
         unique() %>%
         sort()
@@ -69,7 +69,8 @@ function(input, output, session) {
         is.null(input$province) | Province %in% input$province,
         is.null(input$region_1) | Region1 %in% input$region_1,
         is.null(input$region_2) | Region2 %in% input$region_2,
-        is.null(input$winery) | Winery %in% input$winery
+        is.null(input$winery) | Winery %in% input$winery,
+        is.null(input$variety) | Variety %in% input$variety
       )
 
     DT::datatable(df, options = list(lengthMenu = c(5, 30, 50), escape = FALSE))
@@ -83,23 +84,37 @@ function(input, output, session) {
         unique() %>%
         sort()
     }
-    
+
     stillSelected <- isolate(input$histProvince[input$histProvince %in% histProvince])
     updateSelectInput(session, "histProvince", choices = histProvince,
                       selected = stillSelected)
   })
-  
+
   observe({
-    histWinery <- if (is.null(input$histCountry)) character(0) else {
+    histRegion1 <- if (is.null(input$histCountry)) character(0) else {
       cleantable %>%
         filter(Country %in% input$histCountry,
                is.null(input$histProvince) | Province %in% input$histProvince) %>%
-        `$`('Winery') %>%
+        `$`('Region1') %>%
         unique() %>%
         sort()
     }
-    stillSelected <- isolate(input$histWinery[input$histWinery %in% histWinery])
-    updateSelectInput(session, "histWinery", choices = histWinery,
+    stillSelected <- isolate(input$histRegion1[input$histRegion1 %in% histRegion1])
+    updateSelectInput(session, "histRegion1", choices = histRegion1,
+                      selected = stillSelected)
+  })
+  
+  observe({
+    histRegion2 <- if (is.null(input$histCountry)) character(0) else {
+      cleantable %>%
+        filter(Country %in% input$histCountry,
+               is.null(input$histProvince) | Province %in% input$histProvince) %>%
+        `$`('Region2') %>%
+        unique() %>%
+        sort()
+    }
+    stillSelected <- isolate(input$histRegion2[input$histRegion2 %in% histRegion2])
+    updateSelectInput(session, "histRegion2", choices = histRegion2,
                       selected = stillSelected)
   })
   
@@ -108,7 +123,10 @@ function(input, output, session) {
       filter(
         is.null(input$histCountry) | Country %in% input$histCountry,
         is.null(input$histProvince) | Province %in% input$histProvince,
-        is.null(input$histWinery) | Winery %in% input$histWinery
+        is.null(input$histRegion1) | Region1 %in% input$histRegion1,
+        is.null(input$histRegion2) | Region2 %in% input$histRegion2,
+        is.null(input$histWinery) | Winery %in% input$histWinery,
+        is.null(input$histVariety) | Variety %in% input$histVariety
       )
     x = df2$Points
     bins <- seq(min(x), max(x), length.out = 10)
@@ -123,24 +141,131 @@ function(input, output, session) {
       filter(
         is.null(input$histCountry) | Country %in% input$histCountry,
         is.null(input$histProvince) | Province %in% input$histProvince,
-        is.null(input$histWinery) | Winery %in% input$histWinery
+        is.null(input$histRegion1) | Region1 %in% input$histRegion1,
+        is.null(input$histRegion2) | Region2 %in% input$histRegion2,
+        is.null(input$histWinery) | Winery %in% input$histWinery,
+        is.null(input$histVariety) | Variety %in% input$histVariety
       )
     wine.by.year = df3 %>% na.omit() %>% group_by(VintageYear) %>% summarise(avg.price = mean(Price))
     wine.by.year = wine.by.year[order(wine.by.year$VintageYear),]
     ggplot(data = wine.by.year, aes(x = VintageYear, y = avg.price)) + geom_line()
   })
+  # #Google charts demo
+  # yearData <- reactive({
+  #   # Filter to the desired year, and put the columns
+  #   # in the order that Google's Bubble Chart expects
+  #   # them (name, x, y, color, size). Also sort by region
+  #   # so that Google Charts orders and colors the regions
+  #   # consistently.
+  #   df4 = cleantable %>%
+  #     filter(
+  #       VintageYears >= input$VintageYears[1],
+  #       VintageYears <= input$VintageYears[2],
+  #       Points >= input$rangePoints[1],
+  #       Points <= input$rangePoints[2],
+  #       Price >= input$rangePrice[1],
+  #       Price <= input$rangePrice[2],
+  #       is.null(input$chartCountry) | Country %in% input$chartCountry,
+  #       is.null(input$chartProvince) | Province %in% input$chartProvince,
+  #       is.null(input$chartRegion1) | Province %in% input$chartRegion1,
+  #       is.null(input$chartRegion2) | Province %in% input$chartRegion2,
+  #       is.null(input$chartWinery) | Province %in% input$chartWinery,
+  #       is.null(input$chartVariety) | Winery %in% input$chartVariety
+  #     )
+  #   wine.by.year = df4 %>% na.omit() %>% group_by(VintageYear) %>% summarise(avg.price = mean(Price))
+  #   wine.by.year = wine.by.year[order(wine.by.year$VintageYear),]
+  #   
+  #   df41 <- cleantable %.%
+  #     filter(Year == input$VinetageYear) %.%
+  #     select(Country, Points, Price, Years) %.%
+  #     arrange(Region)
+  # })
+  # 
+  # output$chart <- reactive({
+  #   # Return the data and options
+  #   list(
+  #     data = googleDataTable(yearData()),
+  #     options = list(
+  #       title = sprintf(
+  #         "Points vs Price around the World, %s",
+  #         input$year),
+  #       series = series
+  #     )
+  #   )
+  # })
+  # 
+  
+  ### word cloud page
+  observe({
+    cloudProvince <- if (is.null(input$cloudCountry)) character(0) else {
+      filter(cleantable, Country %in% input$cloudCountry) %>%
+        `$`('Province') %>%
+        unique() %>%
+        sort()
+    }
+    
+    stillSelected <- isolate(input$cloudProvince[input$cloudProvince %in% cloudProvince])
+    updateSelectInput(session, "cloudProvince", choices = cloudProvince,
+                      selected = stillSelected)
+  })
+  
+  observe({
+    cloudRegion1 <- if (is.null(input$cloudCountry)) character(0) else {
+      cleantable %>%
+        filter(Province %in% input$cloudProvince,
+               is.null(input$cloudProvince) | Province %in% input$cloudProvince) %>%
+        `$`('Region1') %>%
+        unique() %>%
+        sort()
+    }
+    stillSelected <- isolate(input$cloudRegion1[input$cloudRegion1 %in% cloudRegion1])
+    updateSelectInput(session, "cloudRegion1", choices = cloudRegion1,
+                      selected = stillSelected)
+  })
+  observe({
+    cloudRegion2 <- if (is.null(input$cloudCountry)) character(0) else {
+      cleantable %>%
+        filter(Region1 %in% input$cloudRegion1,
+               is.null(input$cloudProvince) | Province %in% input$cloudProvince) %>%
+        `$`('Region2') %>%
+        unique() %>%
+        sort()
+    }
+    stillSelected <- isolate(input$cloudRegion2[input$cloudRegion2 %in% cloudRegion2])
+    updateSelectInput(session, "cloudRegion2", choices = cloudRegion2,
+                      selected = stillSelected)
+  })
+  observe({
+    cloudWinery <- if (is.null(input$cloudCountry)) character(0) else {
+      cleantable %>%
+        filter(Province %in% input$cloudProvince,
+               is.null(input$cloudProvince) | Province %in% input$cloudProvince) %>%
+        `$`('Winery') %>%
+        unique() %>%
+        sort()
+    }
+    stillSelected <- isolate(input$cloudWinery[input$cloudWinery %in% cloudWinery])
+    updateSelectInput(session, "cloudWinery", choices = cloudWinery,
+                      selected = stillSelected)
+  })
   
   output$wordCloud <- renderPlot({
-    df4 = cleantable %>%
-      filter(
-        is.null(input$histCountry) | Country %in% input$histCountry,
-        is.null(input$histProvince) | Province %in% input$histProvince,
-        is.null(input$histWinery) | Winery %in% input$histWinery
-      )
-    
-    v <- getTermMatrix(df4$Description)
-    wordcloud_rep(names(v), v, scale=c(4,0.5),
-                  min.freq = 10, max.words=20,
-                  colors=brewer.pal(8, "Dark2"))
+
+     df5 = vtable %>%
+       filter(
+         is.null(input$cloudCountry) | Country %in% input$cloudCountry,
+         is.null(input$cloudProvince) | Province %in% input$cloudProvince,
+         is.null(input$cloudRegion1) | Region1 %in% input$cloudRegion1,
+         is.null(input$cloudRegion2) | Region2 %in% input$cloudRegion2,
+         is.null(input$cloudWinery) | Winery %in% input$cloudWinery,
+         is.null(input$cloudVarietry) | Variety %in% input$cloudVariety
+       )
+     wordcloud_rep <- repeatable(wordcloud)
+     v <- getTermMatrix(df5$Description)
+     wordcloud_rep(names(v), v, scale=c(4,0.5),
+                   min.freq = 10, max.words=50,
+                   colors=brewer.pal(8, "Dark2"))
+     #wordcloud(d$word, d$freq,random.order = FALSE, rot.per = 0.1, scale = c(4,.2), max.words = 30, colors = brewer.pal(8, 'Dark2'))
+     title(main = 'world cloud = unigram', font.main = 3, cex.main = 1.5)
   })
 }
--- ui.R
+++ ui.R
@@ -20,36 +20,31 @@ navbarPage("WineDiscovery", id="nav",
                              selectInput("country", "Country", c("All countries"="", sort(unique(as.character(cleantable$Country)))), multiple=TRUE)
                       ),
                       column(2,
-                             conditionalPanel("input.country",
-                                              selectInput("province", "Province", c("All provinces"=""), multiple=TRUE)
-                             )
+                             selectInput("province", "Province", c("All provinces"="", sort(unique(as.character(cleantable$Province)))), multiple=TRUE)
                       ),
                       column(2,
-                             conditionalPanel("input.province",
-                                              selectInput("region_1", "Region1", c("All Region1"=""), multiple=TRUE)
-                             )
+                             selectInput("region_1", "Region1", c("All Region1"="", sort(unique(as.character(cleantable$Region1)))), multiple=TRUE)
                       ),
                       column(2,
-                             conditionalPanel("input.province",
-                                              selectInput("region_2", "Region2", c("All Region2"=""), multiple=TRUE)
-                             )
+                             selectInput("region_2", "Region2", c("All Region2"="", sort(unique(as.character(cleantable$Region2)))), multiple=TRUE)
                       ),
                       column(2,
-                             conditionalPanel("input.province",
-                                              selectInput("winery", "Winery", c("All wineries"=""), multiple=TRUE)
-                             )
+                             selectInput("winery", "Winery", c("All wineries"="",sort(unique(as.character(cleantable$Winery)))), multiple=TRUE)
+                      ),
+                      column(2,
+                             selectInput("variety", "Variety", c("All Varieties"="",sort(unique(as.character(cleantable$Variety)))), multiple=TRUE)
                       )
                     ),
                     fluidRow(
                       column(4,
                              #numericInput("minPoints", "Min points", min=0, max=100, value=0)
-                             sliderInput("rangePoints", label = "Points Range", min = 0, 
+                             sliderInput("rangePoints", label = "Points Range", min = 80, 
                                          max = 100, value = c(60, 80))
                       ),
                       column(4,
                              #numericInput("minPrice", "Min price", min=0, max=100, value=0)
                              sliderInput("rangePrice", label = "Price Range", min = 0, 
-                                         max = max(cleantable$Price, na.rm = T), value = c(10, 50))
+                                         max = 200, value = c(10, 50))
                       )
                   
                     ),
@@ -59,17 +54,43 @@ navbarPage("WineDiscovery", id="nav",
            tabPanel("Wine Visuals",
                     sidebarPanel(
                       selectInput("histCountry", "Country", c("All countries"="", sort(unique(as.character(cleantable$Country)))), multiple=TRUE),
-                      conditionalPanel("input.histCountry",
-                                       selectInput("histProvince", "Province", c("All provinces"=""), multiple=TRUE)
-                      ),
-                      conditionalPanel("input.histProvince",
-                                       selectInput("histWinery", "Winery", c("All wineries"=""), multiple=TRUE)
-                      )
+                      selectInput("histProvince", "Province", c("All provinces"="", sort(unique(as.character(cleantable$Province)))), multiple=TRUE),
+                      selectInput("histRegion1", "Region1", c("All region_1"="", sort(unique(as.character(cleantable$Region1)))), multiple=TRUE),
+                      selectInput("histRegion2", "Region2", c("All region_2"="", sort(unique(as.character(cleantable$Region2)))), multiple=TRUE),
+                      selectInput("histWinery", "Winery", c("All wineries"="", sort(unique(as.character(cleantable$Winery)))), multiple=TRUE),
+                      selectInput("histVariety", "Variety", c("All varieties"="", sort(unique(as.character(cleantable$Variety)))), multiple=TRUE)
                     ),
                     mainPanel(
                       plotOutput('priceGraph'),
-                      plotOutput('hist'),
-                      plotOutput('wordCloud')
+                      plotOutput('hist')
+                      #plotOutput('wordCloud')
                     )
+           # ),
+           # tabPanel("bChart",
+           #          sidebarPanel(
+           #            selectInput("histCountry", "Country", c("All countries"="", sort(unique(as.character(cleantable$Country)))), multiple=TRUE),
+           #            selectInput("histProvince", "Province", c("All provinces"="", sort(unique(as.character(cleantable$Province)))), multiple=TRUE),
+           #            selectInput("histRegion1", "Region1", c("All region_1"="", sort(unique(as.character(cleantable$Region1)))), multiple=TRUE),
+           #            selectInput("histRegion2", "Region2", c("All region_2"="", sort(unique(as.character(cleantable$Region2)))), multiple=TRUE),
+           #            selectInput("histWinery", "Winery", c("All wineries"="", sort(unique(as.character(cleantable$Winery)))), multiple=TRUE),
+           #            selectInput("histVariety", "Variety", c("All varieties"="", sort(unique(as.character(cleantable$Variety)))), multiple=TRUE)
+           #          ),
+           #          mainPanel(
+           #            plotOutput('bubblechart'),
+           #          
+           #          )
+           ),
+           tabPanel("Word Cloud",
+                  sidebarPanel(
+                    selectInput("cloudCountry", "Country", c("All countries"="", sort(unique(as.character(vtable$Country)))), multiple=TRUE),
+                    selectInput("cloudProvince", "Province", c("All provinces"="", sort(unique(as.character(vtable$Province)))), multiple=TRUE),
+                    selectInput("cloudRegion1", "Region1", c("All region_1"="", sort(unique(as.character(vtable$Region1)))), multiple=TRUE),
+                    selectInput("cloudRegion2", "Region2", c("All region_2"="", sort(unique(as.character(vtable$Region2)))), multiple=TRUE),
+                    selectInput("cloudWinery", "Winery", c("All wineries"="", sort(unique(as.character(vtable$Winery)))), multiple=TRUE),
+                    selectInput("cloudVariety", "Variety", c("All varieties"="", sort(unique(as.character(vtable$Variety)))), multiple=TRUE)
+             ),
+             mainPanel(
+               plotOutput('wordCloud')
+             )
            )
 )
